c2e9f2ec9579a1d7cce06dcbbc889804cd61387a
Aadesh Thoppae <athoppae@gmail.com>
2025-01-25 14:40:10 -0800

    init setup - folder setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical ChatBot Project',
+    version = '0.0.0',
+    author = 'Aadesh Thoppae',
+    author_email='athoppae@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
